f83b911ba70ada1742bc3a4440329976fc0b779f
Kyle Weisel <kyle.weisel@gmail.com>
2014-05-13 17:29:44 -0400

    Initial commit

--- android/src/main/java/tv/present/android/mediacore/EncoderConfig.java
+++ android/src/main/java/tv/present/android/mediacore/EncoderConfig.java
@@ -3,5 +3,40 @@ package tv.present.android.mediacore;
 /**
  * Created by kbw28 on 5/13/14.
  */
+
+import android.opengl.EGLContext;
+
+import java.io.File;
+
+/**
+ * Encoder configuration.
+ * <p>
+ * Object is immutable, which means we can safely pass it between threads without
+ * explicit synchronization (and don't need to worry about it getting tweaked out from
+ * under us).
+ * <p>
+ * TODO: make frame rate and iframe interval configurable?  Maybe use builder pattern
+ *       with reasonable defaults for those and bit rate.
+ */
 public class EncoderConfig {
+    final File mOutputFile;
+    final int mWidth;
+    final int mHeight;
+    final int mBitRate;
+    final EGLContext mEglContext;
+
+    public EncoderConfig(File outputFile, int width, int height, int bitRate,
+                         EGLContext sharedEglContext) {
+        mOutputFile = outputFile;
+        mWidth = width;
+        mHeight = height;
+        mBitRate = bitRate;
+        mEglContext = sharedEglContext;
+    }
+
+    @Override
+    public String toString() {
+        return "EncoderConfig: " + mWidth + "x" + mHeight + " @" + mBitRate +
+                " to '" + mOutputFile.toString() + "' ctxt=" + mEglContext;
+    }
 }
