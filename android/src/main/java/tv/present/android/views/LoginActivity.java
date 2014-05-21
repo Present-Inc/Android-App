aac27d66dfa9727b1715b01c82f5c89c26bfee07
Kyle Weisel <kyle.weisel@gmail.com>
2014-05-21 17:28:52 -0400

    Added listener functions

--- android/src/main/java/tv/present/android/views/LoginActivity.java
+++ android/src/main/java/tv/present/android/views/LoginActivity.java
@@ -1,15 +1,10 @@
 package tv.present.android.views;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+
 import tv.present.android.R;
 
 public class LoginActivity extends Activity {
@@ -19,9 +14,7 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+            getFragmentManager().beginTransaction().add(R.id.container, new LoginFragment()).commit();
         }
     }
 
@@ -46,19 +39,4 @@ public class LoginActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_login2, container, false);
-            return rootView;
-        }
-    }
 }
