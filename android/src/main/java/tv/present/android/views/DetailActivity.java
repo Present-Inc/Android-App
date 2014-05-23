ba55040a1c07cd27585f5fe1cc1d9fa74cf51077
Kyle Weisel <kyle.weisel@gmail.com>
2014-05-23 13:05:08 -0400

    Finalized some variables, added method comments

--- android/src/main/java/tv/present/android/views/DetailActivity.java
+++ android/src/main/java/tv/present/android/views/DetailActivity.java
@@ -6,15 +6,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 import tv.present.android.R;
 
-public class DetailActivity extends Activity {
+public final class DetailActivity extends Activity {
 
+    /**
+     * Creates this view.
+     * @param savedInstanceState is a Bundle of data that represents how this view once existed.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
     }
 
-
+    /**
+     * Inflate the menu.
+     * @param menu is a Menu.
+     * @return true
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
@@ -23,11 +31,14 @@ public class DetailActivity extends Activity {
         return true;
     }
 
+    /**
+     * Handles clicks on the action bar.  Home/Up clicks are handled automatically so long as a
+     * parent is specified in the manifest.
+     * @param item is the MenuItem that was clicked.
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
