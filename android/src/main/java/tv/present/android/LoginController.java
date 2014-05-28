e9e88ee98a97beac0077e99c623e9e1287ded3b8
Kyle Weisel <kyle.weisel@gmail.com>
2014-05-28 16:46:38 -0400

    Now uses new model

--- android/src/main/java/tv/present/android/LoginController.java
+++ android/src/main/java/tv/present/android/LoginController.java
@@ -2,29 +2,29 @@ package tv.present.android;
 
 import android.os.AsyncTask;
 
-import tv.present.api.PUserContext;
+import tv.present.models.UserContext;
 
 /**
  * Created by kbw28 on 4/23/14.
  */
-public class LoginController extends AsyncTask<String, Void, PUserContext> {
+public class LoginController extends AsyncTask<String, Void, UserContext> {
 
     public AsyncTaskResponse delegate = null;
 
     @Override
-    public PUserContext doInBackground(String ... params) {
+    public UserContext doInBackground(String ... params) {
 
         String username = params[0];
         String password = params[1];
 
-        PUserContext userContext = PUserContext.create(username, password);
+        UserContext userContext = UserContext.create(username, password);
 
         return userContext;
 
     }
 
     @Override
-    public void onPostExecute(PUserContext userContext) {
+    public void onPostExecute(UserContext userContext) {
         this.delegate.processAsyncResponse(userContext);
     }
 
