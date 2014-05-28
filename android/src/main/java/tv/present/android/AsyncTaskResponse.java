1bb482ccb9d4abf01b6087c81e4be66c2107d0b8
Kyle Weisel <kyle.weisel@gmail.com>
2014-05-28 16:46:11 -0400

    Now uses new model

--- android/src/main/java/tv/present/android/AsyncTaskResponse.java
+++ android/src/main/java/tv/present/android/AsyncTaskResponse.java
@@ -1,10 +1,10 @@
 package tv.present.android;
 
-import tv.present.api.PUserContext;
+import tv.present.models.UserContext;
 
 /**
  * Created by kbw28 on 4/23/14.
  */
 public interface AsyncTaskResponse {
-    public void processAsyncResponse(PUserContext userContext);
+    public void processAsyncResponse(UserContext userContext);
 }
