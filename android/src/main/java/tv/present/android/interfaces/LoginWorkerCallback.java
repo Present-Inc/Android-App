101c57c9a56522e5093a500deda320a759c0e106
Kyle Weisel <kyle.weisel@gmail.com>
2014-05-29 01:55:01 -0400

    Initial commit

---
+++ android/src/main/java/tv/present/android/interfaces/LoginWorkerCallback.java
@@ -0,0 +1,8 @@
+package tv.present.android.interfaces;
+
+/**
+ * Created by kbw28 on 5/29/14.
+ */
+public interface LoginWorkerCallback {
+    public void callbackLogin(Boolean successfulLogin);
+}
