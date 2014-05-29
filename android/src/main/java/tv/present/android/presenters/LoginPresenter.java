0275dcb1c621e9ea885e60662e751343aea6141d
Kyle Weisel <kyle.weisel@gmail.com>
2014-05-28 21:45:59 -0400

    Initial commit

---
+++ android/src/main/java/tv/present/android/presenters/LoginPresenter.java
@@ -0,0 +1,25 @@
+package tv.present.android.presenters;
+
+import tv.present.android.views.NotificationsFragment;
+
+/**
+ * Created by kbw28 on 5/28/14.
+ */
+public class LoginPresenter {
+
+    private static final String TAG = "tv.present.android.controllers.LoginPresenter";
+    private final int REQUEST_CODE_PROFILE_IMAGE_CAPTURE = 8;
+    private NotificationsFragment subject;
+
+    public LoginPresenter(NotificationsFragment fragment) {
+        this.subject = fragment;
+    }
+
+    public void doLogin(String username, String password) {
+
+
+
+    }
+
+
+}
