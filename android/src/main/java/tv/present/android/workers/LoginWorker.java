704cdb9a9ef3027e49cfdf21b5e444b42414cc9e
Kyle Weisel <kyle.weisel@gmail.com>
2014-05-29 01:56:22 -0400

    Interface now defines callback object

--- android/src/main/java/tv/present/android/workers/LoginWorker.java
+++ android/src/main/java/tv/present/android/workers/LoginWorker.java
@@ -2,18 +2,25 @@ package tv.present.android.workers;
 
 import android.os.AsyncTask;
 
+import tv.present.android.interfaces.LoginWorkerCallback;
 import tv.present.android.models.ApplicationCore;
 import tv.present.models.UserContext;
 
 /**
  * Does the work required to perform a user login in the background.
  */
-public class LoginWorker extends AsyncTask<String, Void, Boolean>{
+public class LoginWorker extends AsyncTask<String, Void, Boolean> {
+
+    private LoginWorkerCallback loginWorkerCallback;
+
+    public LoginWorker(LoginWorkerCallback callback) {
+        this.loginWorkerCallback = callback;
+    }
 
     // Inovked on UI before thread is executed
     @Override
     public void onPreExecute() {
-
+        // Do nothing
     }
 
     @Override
@@ -43,9 +50,12 @@ public class LoginWorker extends AsyncTask<String, Void, Boolean>{
 
     }
 
-    // Will execute on UI thread
+    // Will execute on UI thread:
+    //  Check whether the boolean was true or false
+    //  Advance the UI to the main screen on a successful login
     @Override
-    public void onPostExecute(Boolean bool) {
-
+    public void onPostExecute(Boolean successfulLogin) {
+        this.loginWorkerCallback.callbackLogin(successfulLogin);
     }
+
 }
