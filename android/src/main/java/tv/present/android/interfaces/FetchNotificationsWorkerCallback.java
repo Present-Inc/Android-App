99f6acb24f6d52b19cfd7dc8c97418cc35a78ee1
Kyle Weisel <kyle.weisel@gmail.com>
2014-05-30 09:27:46 -0400

    Initial commit

---
+++ android/src/main/java/tv/present/android/interfaces/FetchNotificationsWorkerCallback.java
@@ -0,0 +1,10 @@
+package tv.present.android.interfaces;
+
+import tv.present.models.ResultSetActivities;
+
+/**
+ * Created by kbw28 on 5/29/14.
+ */
+public interface FetchNotificationsWorkerCallback {
+    public void callbackFetchNotifications(ResultSetActivities resultSet);
+}
