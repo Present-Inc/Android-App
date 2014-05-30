c96b49dea8ca903702c0e570722674e081e29555
Kyle Weisel <kyle.weisel@gmail.com>
2014-05-30 13:31:13 -0400

    Updated JavaDoc

--- android/src/main/java/tv/present/android/interfaces/CreateAccountWorkerCallback.java
+++ android/src/main/java/tv/present/android/interfaces/CreateAccountWorkerCallback.java
@@ -3,7 +3,8 @@ package tv.present.android.interfaces;
 import tv.present.models.User;
 
 /**
- * This interface defines the callback method used by the create account worker.
+ *  This interface defines the callback method used by the CreateAccountWorker.
+ *  @author Kyle Weisel (kyle@present.tv)
  */
 public interface CreateAccountWorkerCallback {
     public void callbackCreateAccount(User user);
