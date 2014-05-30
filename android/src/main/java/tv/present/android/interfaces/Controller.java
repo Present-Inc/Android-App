2b97d0c88b6f468d94e99513e2ca21ef0a65832a
Kyle Weisel <kyle.weisel@gmail.com>
2014-05-30 13:31:05 -0400

    Updated JavaDoc

--- android/src/main/java/tv/present/android/interfaces/Controller.java
+++ android/src/main/java/tv/present/android/interfaces/Controller.java
@@ -1,7 +1,8 @@
 package tv.present.android.interfaces;
 
 /**
- * Created by kbw28 on 5/29/14.
+ *  This Controller is an abstract class that all Controllers will inherit from.
+ *  @author Kyle Weisel (kyle@present.tv)
  */
 public abstract class Controller {
 }
