284e199af0cd32509298c826e0c6f78b832f02d1
Kyle Weisel <kyle.weisel@gmail.com>
2014-05-30 09:28:05 -0400

    Initial commit

---
+++ android/src/main/java/tv/present/android/interfaces/UpdateUserDetailsWorkerCallback.java
@@ -0,0 +1,8 @@
+package tv.present.android.interfaces;
+
+/**
+ * This interface defines the callback method used by the update user details worker.
+ */
+public interface UpdateUserDetailsWorkerCallback {
+    public void callbackUpdateUserDetails(boolean successfulUpdate);
+}
