1c4b7ad6e05a333b55e918c3676c362b50e501b9
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-11 13:45:03 -0400

    Implemented threadCallback() method for fetch notifications

--- android/src/main/java/tv/present/android/views/NotificationsView.java
+++ android/src/main/java/tv/present/android/views/NotificationsView.java
@@ -15,6 +15,8 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 import tv.present.android.R;
 import tv.present.android.controllers.CoreController;
 import tv.present.android.controllers.PController;
@@ -24,6 +26,8 @@ import tv.present.android.models.PView;
 import tv.present.android.util.PAndroidUtils;
 import tv.present.android.util.PLog;
 import tv.present.android.workers.DownloadImageWorker;
+import tv.present.models.PUserActivity;
+import tv.present.util.PResultSet;
 
 /**
  * Present Notifications View
@@ -180,6 +184,36 @@ public class NotificationsView extends PView implements View.OnFocusChangeListen
     }
 
     public void threadCallback(PCallbackResult callbackData) {
+        public void callbackFetchNotifications(PResultSet < PUserActivity > resultSet) {
+            // Loop through the notifications and update the view here
 
+            ArrayList<PUserActivity> results = resultSet.getResults();
+
+            PLog.logNotice(TAG, "Results array has a size of " + results.size());
+
+
+            final int size = results.size();
+            for (int i = 0; i < size; i++) {
+                PLog.logNotice(TAG, "Looping through the results " + i);
+                PUserActivity userActivity = results.get(i);
+                if (userActivity == null) {
+                    PLog.logError(TAG, "The user activity " + i + " was null!");
+
+                    PUserActivity result2 = results.get(i+1);
+                    if (result2 == null) {
+                        PLog.logError(TAG, "The user activity " + i+1 + " was also null!");
+                    }
+                    else {
+                        PLog.logNotice(TAG, "The user activity " + (i+1) + " was not null, however.  In fact " + result2.getSourceUser().getUsername());
+                    }
+                }
+                else {
+                    PLog.logNotice(TAG, "The user activity " + i + " was not null!");
+                }
+                String message = userActivity.getSubject();
+                String userProfileImageURL = userActivity.getSourceUser().getProfile().getProfilePictureURL();
+                this.fragment.addNotification(userProfileImageURL, message);
+            }
+        }
     }
 }
