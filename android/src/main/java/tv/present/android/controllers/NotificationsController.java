501a06c04f8c63cda69fe98c7cb041fc371751d8
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-10 23:14:29 -0400

    Now extends PController

--- android/src/main/java/tv/present/android/controllers/NotificationsController.java
+++ android/src/main/java/tv/present/android/controllers/NotificationsController.java
@@ -4,15 +4,14 @@ import android.app.Fragment;
 
 import java.util.ArrayList;
 
-import tv.present.android.interfaces.Controller;
 import tv.present.android.interfaces.FetchNotificationsWorkerCallback;
 import tv.present.android.util.PLog;
 import tv.present.android.views.NotificationsFragment;
 import tv.present.android.workers.FetchNotificationsWorker;
-import tv.present.models.PResultSet;
-import tv.present.models.UserActivity;
+import tv.present.models.PUserActivity;
+import tv.present.util.PResultSet;
 
-public class NotificationsController extends Controller implements FetchNotificationsWorkerCallback {
+public class NotificationsController extends PController implements FetchNotificationsWorkerCallback {
 
     private static final String TAG = "tv.present.android.controllers.NotificationsController";
     private final NotificationsFragment fragment;
@@ -25,10 +24,10 @@ public class NotificationsController extends Controller implements FetchNotifica
         return this.fragment;
     }
 
-    public void callbackFetchNotifications(PResultSet<UserActivity> resultSet) {
+    public void callbackFetchNotifications(PResultSet<PUserActivity> resultSet) {
         // Loop through the notifications and update the view here
 
-        ArrayList<UserActivity> results = resultSet.getResults();
+        ArrayList<PUserActivity> results = resultSet.getResults();
 
         PLog.logNotice(TAG, "Results array has a size of " + results.size());
 
@@ -36,11 +35,11 @@ public class NotificationsController extends Controller implements FetchNotifica
         final int size = results.size();
         for (int i = 0; i < size; i++) {
             PLog.logNotice(TAG, "Looping through the results " + i);
-            UserActivity userActivity = results.get(i);
+            PUserActivity userActivity = results.get(i);
             if (userActivity == null) {
                 PLog.logError(TAG, "The user activity " + i + " was null!");
 
-                UserActivity result2 = results.get(i+1);
+                PUserActivity result2 = results.get(i+1);
                 if (result2 == null) {
                     PLog.logError(TAG, "The user activity " + i+1 + " was also null!");
                 }
