417dc463ebeeb0153e54be801c0f90786f032cf2
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-10 23:14:49 -0400

    Initial commit

---
+++ android/src/main/java/tv/present/android/controllers/PController.java
@@ -0,0 +1,14 @@
+package tv.present.android.controllers;
+
+import android.app.Activity;
+
+import java.io.Serializable;
+
+/**
+ * Present Controller Abstract Object
+ * June 04, 2014
+ * @author Kyle Weisel (kyle@present.tv)
+ */
+public abstract class PController extends Activity implements Serializable {
+
+}
