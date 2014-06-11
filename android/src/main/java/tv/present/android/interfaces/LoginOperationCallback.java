86ed5e9eb125cd4060037db7a325dd020610bb5e
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-10 23:19:19 -0400

    Initial commit

---
+++ android/src/main/java/tv/present/android/interfaces/LoginOperationCallback.java
@@ -0,0 +1,8 @@
+package tv.present.android.interfaces;
+
+/**
+ * Defines the callback method for a login operation.
+ */
+public interface LoginOperationCallback {
+    public void callbackLoginOperation(boolean successfulLogin);
+}
