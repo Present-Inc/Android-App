adab568a37d8ed1757aea573dcca6b0c2ba448a3
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-10 23:15:22 -0400

    Added methods to handle click and touch

--- android/src/main/java/tv/present/android/interfaces/Controller.java
+++ android/src/main/java/tv/present/android/interfaces/Controller.java
@@ -1,8 +1,13 @@
 package tv.present.android.interfaces;
 
+import android.view.MotionEvent;
+import android.view.View;
+
 /**
  *  This Controller is an abstract class that all Controllers will inherit from.
  *  @author Kyle Weisel (kyle@present.tv)
  */
-public abstract class Controller {
+public interface Controller {
+    public void handleClick(View view);
+    public void handleTouch(View view, MotionEvent event);
 }
