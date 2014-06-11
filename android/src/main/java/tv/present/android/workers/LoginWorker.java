52aea3a39e154e83ba9c6066f2b87540557b1a81
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-10 23:26:36 -0400

    Preparing to delete this worker

--- android/src/main/java/tv/present/android/workers/LoginWorker.java
+++ android/src/main/java/tv/present/android/workers/LoginWorker.java
@@ -4,12 +4,13 @@ import android.os.AsyncTask;
 
 import tv.present.android.interfaces.LoginWorkerCallback;
 import tv.present.android.models.ApplicationCore;
-import tv.present.models.UserContext;
+import tv.present.api.PAPIInteraction;
+import tv.present.models.PUserContext;
 
 /**
  * Does the work required to perform a user login in the background.
  */
-public class LoginWorker extends AsyncTask<String, Void, Boolean> {
+    public class LoginWorker extends AsyncTask<String, Void, Boolean> {
 
     private LoginWorkerCallback loginWorkerCallback;
 
@@ -21,33 +22,6 @@ public class LoginWorker extends AsyncTask<String, Void, Boolean> {
     @Override
     public void onPreExecute() {
         // Do nothing
-    }
-
-    @Override
-    public Boolean doInBackground(String ... params) {
-
-        ApplicationCore appCore = ApplicationCore.getInstance();
-        UserContext userContext;
-
-        // Check if for some reason we have a user context
-        if (appCore.getUserContext() != null) {
-            userContext = appCore.getUserContext();
-        }
-        // Otherwise, try to get one (ie: do the login)
-        else {
-            String username = params[0];
-            String password = params[1];
-            userContext = UserContext.create(username, password);   // Could be null
-        }
-
-        // If the userContext came back not null, set it in the app core.
-        if (userContext != null)  {
-            appCore.setUserContext(userContext);
-            return true;
-        }
-
-        return false;
-
     }
 
     // Will execute on UI thread:
