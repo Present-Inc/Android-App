d278147a8e11cdf5badc51faa8d40272b43e0bad
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-10 23:19:36 -0400

    Initial commit

---
+++ android/src/main/java/tv/present/android/interfaces/ThreadCallback.java
@@ -0,0 +1,7 @@
+package tv.present.android.interfaces;
+
+import tv.present.android.models.PCallbackResult;
+
+public interface ThreadCallback {
+    public void threadCallback(PCallbackResult<?> callbackResult);
+}
