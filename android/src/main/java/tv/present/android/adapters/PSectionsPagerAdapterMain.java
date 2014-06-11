c6538687dc0b2deb43cf55d7ab2b61274e26b61b
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-11 16:47:23 -0400

    Initial commit

---
+++ android/src/main/java/tv/present/android/adapters/PSectionsPagerAdapterMain.java
@@ -0,0 +1,63 @@
+package tv.present.android.adapters;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+
+import tv.present.android.R;
+import tv.present.android.controllers.CoreController;
+import tv.present.android.controllers.PController;
+import tv.present.android.views.HomeFeedView;
+import tv.present.android.views.PPlaceholderView;
+
+/**
+ * Created by kbw28 on 6/11/14.
+ */
+public final class PSectionsPagerAdapterMain extends PSectionsPagerAdapter {
+
+    public PSectionsPagerAdapterMain(FragmentManager fragmentManager, PController controller) {
+        super(fragmentManager, controller);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        final Fragment returnValue;
+
+        // Switch on the tab position that we need a fragment for.
+        switch (position) {
+            case 0:
+                returnValue = HomeFeedView.newInstance(this.controller);
+                break;
+            case 1:
+                returnValue = ((CoreController) this.controller).getNotificationsView();
+                break;
+            default:
+                returnValue = PPlaceholderView.newInstance(position + 1);
+                break;
+        }
+
+        return returnValue;
+    }
+
+    @Override
+    public int getCount() {
+        // Show 4 total pages.
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return this.controller.getString(R.string.title_section_1);
+            case 1:
+                return this.controller.getString(R.string.title_section_2);
+            case 2:
+                return this.controller.getString(R.string.title_section_3);
+            case 3:
+                return this.controller.getString(R.string.title_section_4);
+        }
+        return null;
+    }
+
+}
