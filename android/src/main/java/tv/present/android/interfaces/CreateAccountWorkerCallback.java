6c6d27baf48df8f17e9cc77566f0250b97b0565c
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-10 23:16:03 -0400

    Changed the callback parameter type

--- android/src/main/java/tv/present/android/interfaces/CreateAccountWorkerCallback.java
+++ android/src/main/java/tv/present/android/interfaces/CreateAccountWorkerCallback.java
@@ -1,11 +1,11 @@
 package tv.present.android.interfaces;
 
-import tv.present.models.User;
+import tv.present.models.PUser;
 
 /**
  *  This interface defines the callback method used by the CreateAccountWorker.
  *  @author Kyle Weisel (kyle@present.tv)
  */
 public interface CreateAccountWorkerCallback {
-    public void callbackCreateAccount(User user);
+    public void callbackCreateAccount(PUser user);
 }
