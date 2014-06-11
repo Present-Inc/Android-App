c0ce9b8fef70c0c4f0927c4e6813d86bbef21f69
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-11 13:42:27 -0400

    Removed default commenting

--- android/src/main/java/tv/present/android/adapters/PSectionsPagerAdapter.java
+++ android/src/main/java/tv/present/android/adapters/PSectionsPagerAdapter.java
@@ -31,8 +31,6 @@ public class PSectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
 
         final Fragment returnValue;
 
