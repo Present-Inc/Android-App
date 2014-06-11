01e9abdcdc0201b2db56e3836fcb0f818267208e
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-11 01:29:36 -0400

    Refined documentation

--- android/src/main/java/tv/present/android/controllers/PController.java
+++ android/src/main/java/tv/present/android/controllers/PController.java
@@ -9,6 +9,18 @@ import java.io.Serializable;
  * June 04, 2014
  * @author Kyle Weisel (kyle@present.tv)
  */
+
+/**
+ * Present Controller Abstract Object
+ *
+ * This object is an abstract superclass of all of the controllers for this application.  A
+ * {@link tv.present.android.controllers.PController} extends an {@link android.app.Activity},
+ * so that all controllers are also Activities.
+ *
+ * June 10, 2014
+ *
+ * @author  Kyle Weisel (kyle@present.tv)
+ */
 public abstract class PController extends Activity implements Serializable {
 
 }
