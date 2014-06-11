be1b384e8093dfff14d4f69eac927db590a702eb
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-10 23:12:22 -0400

    Initial commit

---
+++ android/src/main/java/tv/present/android/adapters/PSectionsPagerAdapter.java
@@ -0,0 +1,76 @@
+package tv.present.android.adapters;
+
+/**
+ * Created by kbw28 on 6/10/14.
+ */
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v13.app.FragmentPagerAdapter;
+
+import tv.present.android.R;
+import tv.present.android.controllers.PController;
+import tv.present.android.views.HomeFeedView;
+import tv.present.android.views.NotificationsFragment;
+import tv.present.android.views.PPlaceholderView;
+
+/**
+ * A {@link android.support.v13.app.FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class PSectionsPagerAdapter extends FragmentPagerAdapter {
+
+    private final PController controller;
+
+    public PSectionsPagerAdapter(FragmentManager fragmentManager, PController controller) {
+        super(fragmentManager);
+        this.controller = controller;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+
+        final Fragment returnValue;
+
+        // Switch on the tab position that we need a fragment for.
+        switch (position) {
+            case 0:
+                returnValue = HomeFeedView.newInstance(this.controller);
+                break;
+            case 1:
+                returnValue = NotificationsFragment.newInstance();
+                break;
+            default:
+                returnValue = PPlaceholderView.newInstance(position + 1);
+                break;
+        }
+
+        return returnValue;
+
+
+    }
+
+    @Override
+    public int getCount() {
+        // Show 4 total pages.
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return this.controller.getString(R.string.title_section_1);
+            case 1:
+                return this.controller.getString(R.string.title_section_2);
+            case 2:
+                return this.controller.getString(R.string.title_section_3);
+            case 3:
+                return this.controller.getString(R.string.title_section_4);
+        }
+        return null;
+    }
+
+}
