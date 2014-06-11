28307ef949102c53f4fc942d4f78c10f37cfd531
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-10 23:22:08 -0400

    Initial commit

---
+++ android/src/main/java/tv/present/android/util/PCallbackIdentifiers.java
@@ -0,0 +1,14 @@
+package tv.present.android.util;
+
+/**
+ * Created by kbw28 on 6/10/14.
+ */
+public class PCallbackIdentifiers {
+
+    public static final int LOGIN = 1;
+    public static final int CREATE_ACCOUNT = 2;
+    public static final int FORGOT_PASSWORD = 4;
+    public static final int LOGIN_AFTER_CREATION = 8;
+    public static final int FETCH_NOTIFICATIONS = 16;
+
+}
