063d87d2cd7b18c8e5772b8841a8d023ef792216
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-13 16:26:16 -0400

    Simplified IF to remove compiler warning

--- android/src/main/java/tv/present/android/activities/DemoActivity.java
+++ android/src/main/java/tv/present/android/activities/DemoActivity.java
@@ -52,12 +52,8 @@ public final class DemoActivity extends Activity implements View.OnClickListener
      */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
     /**
