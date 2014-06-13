4110a0ab86abe73b379c68ed270f3e9acf2a49a1
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-13 02:57:03 -0400

    Initial commit

---
+++ android/src/main/java/tv/present/android/adapters/NotificationsAdapter.java
@@ -0,0 +1,78 @@
+package tv.present.android.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import tv.present.android.R;
+import tv.present.android.models.PView;
+import tv.present.android.views.CircularImageView;
+import tv.present.models.PUserActivity;
+import tv.present.util.PResultSet;
+
+/**
+ * Created by kbw28 on 6/12/14.
+ */
+public class NotificationsAdapter extends BaseAdapter {
+
+    private static final String TAG = "tv.present.android.adapters.NotificationsAdapter";
+    private final LayoutInflater layoutInflater;
+    private PResultSet<PUserActivity> userActivitiesResultSet;
+    private PView parentView;
+
+    public NotificationsAdapter(Context context, PResultSet<PUserActivity> userActivitiesResultSet, PView parentView) {
+        this.layoutInflater = LayoutInflater.from(context);
+        this.userActivitiesResultSet = userActivitiesResultSet;
+        this.parentView = parentView;
+    }
+
+    public void setData(PResultSet<PUserActivity> userActivitiesResultSet) {
+        if (this.userActivitiesResultSet != null) {
+            this.userActivitiesResultSet = null;
+        }
+        this.userActivitiesResultSet = userActivitiesResultSet;
+        notifyDataSetChanged();
+    }
+
+    public View getView(int i, View view, ViewGroup parent) {
+
+        PUserActivity userActivity = (PUserActivity) this.getItem(i);
+
+        if (view == null) {
+            view = this.layoutInflater.inflate(R.layout.detail_notification, null, false);
+        }
+
+        view.setOnClickListener(this.parentView);
+        view.setOnLongClickListener(this.parentView);
+
+        //ImageView tableRowHR = (ImageView) inflater.inflate(R.layout.imageview_hr, null, false);
+
+        TextView tableRowTextView = (TextView) view.findViewById(R.id.notificationTextView);
+        tableRowTextView.setText(userActivity.getSubject());
+
+        CircularImageView circularImageView = (CircularImageView) view.findViewById(R.id.notificationProfileImage);
+        //AsyncTask downloadImageworker = new DownloadImageWorker(circularImageView);
+
+        return view;
+
+    }
+
+    @Override
+    public int getCount() {
+        return this.userActivitiesResultSet.getResults().size();
+    }
+
+    @Override
+    public Object getItem(final int i) {
+        return this.userActivitiesResultSet.getResults().get(i);
+    }
+
+    @Override
+    public long getItemId(final int i) {
+        return i;
+    }
+
+}
