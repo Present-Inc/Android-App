91b591804c485a8a2105589aa7f9d3da0d786a74
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-13 03:06:47 -0400

    Initial commit

---
+++ android/src/main/java/tv/present/android/adapters/PAdapter.java
@@ -0,0 +1,17 @@
+package tv.present.android.adapters;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Present Abstract Base Adapter
+ *
+ * This {@link tv.present.android.adapters.PAdapter} is an abstract class for adapters used in the
+ * Present application.  This class will need to be subclassed and implemented by a concreate
+ * adapter.
+ *
+ * June 13, 2014
+ *
+ * @author  Kyle Weisel (kyle@present.tv)
+ */
+public abstract class PAdapter extends BaseAdapter {
+}
