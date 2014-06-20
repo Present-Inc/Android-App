0fddc9733f62fe4a194dd6c182aa6e83ab135c8c
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-19 22:22:16 -0400

    Initial commit

---
+++ android/src/main/java/tv/present/android/threads/AudioRecordRunnable.java
@@ -0,0 +1,78 @@
+package tv.present.android.threads;
+
+import tv.present.android.util.PChunkingRecorder;
+import tv.present.android.util.PLog;
+
+/**
+ * Present Audio Record Runnable
+ *
+ * This class is the runnable thread for the audio recorder.
+ *
+ * June 19, 2014
+ * @author Kyle Weisel (kyle@present.tv)
+ *
+ */
+public class AudioRecordRunnable implements Runnable {
+
+    private static final String TAG = "tv.present.android.threads.AudioRecordRunnable";
+    private PChunkingRecorder chunkingRecorder;
+
+    /**
+     * Constructs an AudioRecordThread.
+     * @param chunkingRecorder is a PChunkingRecorder (which is probably a reference to the object
+     *                         that created this thread)
+     */
+    public AudioRecordRunnable(final PChunkingRecorder chunkingRecorder) {
+        this.chunkingRecorder = chunkingRecorder;
+    }
+
+    /**
+     * This method is called when the thread is started.
+     */
+    @Override
+    public void run() {
+
+        this.chunkingRecorder.getAudioRecord().startRecording();
+        boolean audioEosRequestedCopy, fullStopReceivedCopy;
+
+        while(true) {
+
+            if (! this.chunkingRecorder.isFirstFrameReady()) {
+                continue;
+            }
+
+            // Creating a copy of this boolean so that it does not change within the loop.
+            audioEosRequestedCopy = this.chunkingRecorder.isAudioEosRequested();
+            fullStopReceivedCopy = this.chunkingRecorder.isFullStopReceived();
+
+            if (audioEosRequestedCopy || fullStopReceivedCopy) {
+                // TODO: Should we post an EOSRecieved message to the handler?
+                PLog.logDebug(TAG, "run() -> Audio loop caught audioEosRequested / fullStopReceived " + audioEosRequestedCopy + " " + fullStopReceivedCopy);
+                this.chunkingRecorder.sendAudioToEncoder(true);
+            }
+
+            if (fullStopReceivedCopy) {
+                PLog.logDebug(TAG, "run() -> Stopping AudioRecord");
+                this.chunkingRecorder.getAudioRecord().stop();
+            }
+
+            synchronized (this.chunkingRecorder.getAudioTrackInfo().muxerWrapper.sync) {
+                this.chunkingRecorder.drainEncoder(this.chunkingRecorder.getAudioEncoder(), this.chunkingRecorder.getAudioBufferInfo(), this.chunkingRecorder.getAudioTrackInfo(), audioEosRequestedCopy || fullStopReceivedCopy);
+            }
+
+            if (audioEosRequestedCopy) {
+                this.chunkingRecorder.setAudioEOSRequested(false);
+            }
+
+            if (! fullStopReceivedCopy) {
+                this.chunkingRecorder.sendAudioToEncoder(false);
+            }
+            else {
+                break;
+            }
+
+        }
+
+    }
+
+}
