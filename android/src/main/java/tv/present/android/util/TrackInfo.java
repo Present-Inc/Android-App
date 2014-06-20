d863b510217de1486793405120ef84d92a98137f
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-19 22:25:41 -0400

    Initial commit

---
+++ android/src/main/java/tv/present/android/util/TrackInfo.java
@@ -0,0 +1,15 @@
+package tv.present.android.util;
+
+/**
+ * Present Track Info
+ *
+ * This class is a simple wrapper to manage MediaMuxerWrapper state.
+ *
+ * June 19, 2014
+ * @author Kyle Weisel (kyle@present.tv)
+ *
+ */
+public class TrackInfo {
+    public int index = 0;
+    public MediaMuxerWrapper muxerWrapper;
+}
