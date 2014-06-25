062ec94af7bf459c05546046c021a9ac2050b699
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-25 03:15:19 -0400

    Added camera handler and camera renderer keys

--- android/src/main/java/tv/present/android/util/PKeys.java
+++ android/src/main/java/tv/present/android/util/PKeys.java
@@ -5,6 +5,8 @@ package tv.present.android.util;
  */
 public final class PKeys {
 
+    public static final String KEY_CAMERA_HANDLER = "camera_handler";
+    public static final String KEY_CAMERA_RENDERER = "camera_renderer";
     public static final String KEY_CONTROLLER = "controller";
     public static final String KEY_CURSOR = "cursor";
     public static final String KEY_LIMIT = "limit";
