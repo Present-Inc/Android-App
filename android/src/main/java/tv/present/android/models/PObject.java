dc876c864a2c689840dcf027bbbf1be6093aab27
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-25 03:14:51 -0400

    Implementing the Serializable interface is required to pass this object through a Bundle

--- android/src/main/java/tv/present/android/models/PObject.java
+++ android/src/main/java/tv/present/android/models/PObject.java
@@ -1,8 +1,10 @@
 package tv.present.android.models;
 
+import java.io.Serializable;
+
 /**
  * Created by kbw28 on 6/9/14.
  */
-public abstract class PObject extends Object {
+public abstract class PObject extends Object implements Serializable {
 
 }
