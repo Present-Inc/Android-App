b4dd63ee508505bbb286b588299789bc0b6c4118
Kyle Weisel <kyle.weisel@gmail.com>
2014-07-01 23:18:10 -0400

    Added private member Camera

--- android/src/main/java/tv/present/android/views/PGLSurfaceView.java
+++ android/src/main/java/tv/present/android/views/PGLSurfaceView.java
@@ -1,19 +1,23 @@
 package tv.present.android.views;
 
 import android.content.Context;
+import android.hardware.Camera;
 import android.opengl.GLSurfaceView;
 import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 
 import tv.present.android.mediacore.PCameraRenderer;
 import tv.present.android.util.PAndroidGlobals;
+import tv.present.android.util.PLog;
 
 /**
  * Created by kbw28 on 6/26/14.
  */
 public class PGLSurfaceView extends GLSurfaceView {
 
+    private static final String TAG = "tv.present.android.views.PGLSurfaceView";
     private PCameraRenderer renderer;
+    private Camera camera;
 
     public PGLSurfaceView(Context context) {
         super(context);
@@ -26,22 +30,29 @@ public class PGLSurfaceView extends GLSurfaceView {
     }
 
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        PLog.logDebug(TAG, "onSurfaceCreated() -> Start method");
         super.surfaceCreated(surfaceHolder);
     }
 
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        PLog.logDebug(TAG, "onSurfaceDestroyed() -> Start method");
         super.surfaceDestroyed(surfaceHolder);
     }
 
     public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int w, int h) {
+        PLog.logDebug(TAG, "onSurfaceChanged() -> Start method");
         super.surfaceChanged(surfaceHolder, format, w, h);
     }
 
     private void initialize() {
+        PLog.logDebug(TAG, "initialize() -> Start method");
+        //this.camera = Camera.open();
         this.renderer = new PCameraRenderer(this.getContext());
+        //this.renderer.setCamera(this.camera);
         this.setEGLContextClientVersion(PAndroidGlobals.EGL_CONTEXT_CLIENT_VERSION);
         this.setRenderer(this.renderer);
         this.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+
     }
 
 }
